04a9f3aedf6ffe8ca2d3b79d2b6f23aeedd52f2b
Nicolas Lefeld <54159298+NicolasLefeld@users.noreply.github.com>
2025-03-11 19:14:38 -0300

    rollback

--- src/utils/service/api-client.ts
+++ src/utils/service/api-client.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { Navigate } from "react-router";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-    url: API_BASE_URL,
+    baseURL: API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
