217cb6d1a71a0f231a379f78e8a8a683e41d7e1c
Nicolas Lefeld <54159298+NicolasLefeld@users.noreply.github.com>
2025-03-11 19:33:09 -0300

    test: moved from baseurl to url

--- src/utils/service/api-client.ts
+++ src/utils/service/api-client.ts
@@ -4,7 +4,7 @@ import { Navigate } from "react-router";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    url: API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
