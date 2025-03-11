e138130bf5b489d34fff886d4884aab6357e7e89
Nicolas Lefeld <54159298+NicolasLefeld@users.noreply.github.com>
2025-03-10 23:48:53 -0300

    Test: remove or conditional on env

--- src/utils/service/api-client.ts
+++ src/utils/service/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Navigate } from "react-router";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "asdas";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
     baseURL: API_BASE_URL,
